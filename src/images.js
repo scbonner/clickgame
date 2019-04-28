b82c41fa47c6e2a6125d0b6729cea75a854f5b4f
scbonner <codingbootcamp918@gmail.com>
2019-04-28 18:29:27 -0400

    added image pics looney toons

---
+++ src/images.js
@@ -0,0 +1,25 @@
+import Bugs from './images/Bugs.png';
+import Daffy from './images/Daffy.png';
+import Foghorn from './images/Foghorn.png';
+import PePe from './images/PePe.jpeg';
+import RoadRunner from './images/RoadRunner.jpeg';
+import  Taz from './images/Taz.jpeg';
+import Tweety from './images/Tweey.jpeg';
+import WileE from './images/WileE.png';
+
+
+
+[
+Bugs,
+Daffy,
+Foghorn,
+PePe,
+RoadRunner,
+Taz,
+Tweety,
+WileE
+
+ ];
+
+ export default images;
+  
