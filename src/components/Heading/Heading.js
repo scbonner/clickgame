f1d8263a37bd2c8890ff0616ff5d2fc8df1db86d
scbonner <codingbootcamp918@gmail.com>
2019-04-28 16:37:47 -0400

    adding heading

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Heading.css";
+
+const Heading = () => (
+    <div className="heading text-center d-flex align-items-center justified-content-center">
+        <p className="m-0">Click image to earn points. Do not click more than once!</p>
+    </div>
+    
+);
+
+export default Heading;
